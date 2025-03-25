f3bf3459525d7167411b29cb313e7cc779e9167e
Kerigan <kerigancreighton@gmail.com>
2025-03-25 10:15:15 -0500

    shhhh

--- build/docker-ota-build.sh
+++ build/docker-ota-build.sh
@@ -12,8 +12,22 @@ if [[ ! -d anki/anki-ble ]]; then
 fi
 
 function usage() {
+	if [[ ${DO_SIGN} == "1" ]]; then
+		usagesign
+	else
+		usagenosign
+	fi
+}
+
+function usagenosign() {
     echo "Usage: ./build/docker-ota-build.sh <oskr/dev> <incremental> <oskr boot image signing password>"
     echo "example: ./build/docker-ota-build.sh dev 6079 <password> <password>"
+    exit 0
+}
+
+function usagesign() {
+    echo "Usage: ./build/docker-ota-build.sh <prod/oskr/dev> <incremental> <ota_key_password> <prod/oskr boot image signing password>"
+    echo "example: ./build/docker-ota-build.sh prod 6079 <password> <password>"
     exit 0
 }
 
@@ -29,13 +43,26 @@ if [[ ! $2 =~ ^-?[0000-9999]+$ ]]; then
     usage
 fi
 
-if [[ $3 == "" && $1 != "dev" ]]; then
-    usage
-fi
+if [[ ${DO_SIGN} == "1" ]]; then
+	if [[ $3 == "" ]]; then
+		usage
+	fi
 
-PRODorOSKR=$1
-INCREMENT=$2
-BOOT_SIGNING_PASSWORD=$3
+	if [[ $4 == "" && $1 != "dev" ]]; then
+		usage
+	fi
+        PRODorOSKR=$1
+        INCREMENT=$2
+	OTA_SIGNING_PASSWORD=$3
+        BOOT_SIGNING_PASSWORD=$4
+else
+	if [[ $3 == "" && $1 != "dev" ]]; then
+		usage
+	fi
+	PRODorOSKR=$1
+	INCREMENT=$2
+	BOOT_SIGNING_PASSWORD=$3
+fi
 
 if [[ ! -d build/cache/ccache ]]; then
     mkdir -p build/cache/ccache
@@ -95,6 +122,17 @@ fi
 
 #&& bitbake -c clean victor && bitbake -c cleansstate victor
 
+if [[ ${DO_SIGN} == "1" ]]; then
+	if openssl rsa -in ota/ota_prod.key -passin pass:"$OTA_SIGNING_PASSWORD" -noout 2>/dev/null; then
+		echo "OTA signing key password is confirmed to be correct!"
+	else
+		echo
+		echo -e "\033[1;31mOTA signing key is incorrect. exiting.\033[0m"
+		echo
+		exit 1
+	fi
+fi
+
 BUILD_ARGS_TERM="-it"
 if [[ ${NO_TTY} == "1" ]]; then
 	BUILD_ARGS_TERM="-t"
@@ -110,7 +148,7 @@ docker run ${BUILD_ARGS_TERM} \
     -v "$(pwd):/home/build/vicos-oelinux" \
     -v "$(pwd)/anki-deps:/home/build/.anki" \
     -v "$(pwd)/build/cache/ccache:/home/build/.ccache" \
-    victor-builder bash -c "cd ~/vicos-oelinux/poky && rm -rf build/tmp-glibc/deploy/images/apq8009-robot-robot-perf/* && source build/conf/set_bb_env.sh && export ANKI_BUILD_VERSION=$INCREMENT && source ~/vicos-oelinux/poky/build/conf/set_anki_build_version.sh && setenv-8009-robot-image$PERForUSER && bitbake -c cleanall victor anki-version rampost base-files wired iptables machine-robot-image core-image-anki-initramfs && $ToDo && export OTA_MANIFEST_SIGNING_KEY=$OTA_SIGNING_PASSWORD && ANKIDEV=$ADEV make && exit 0"
+    victor-builder bash -c "cd ~/vicos-oelinux/poky && rm -rf build/tmp-glibc/deploy/images/apq8009-robot-robot-perf/* && source build/conf/set_bb_env.sh && export ANKI_BUILD_VERSION=$INCREMENT && source ~/vicos-oelinux/poky/build/conf/set_anki_build_version.sh && setenv-8009-robot-image$PERForUSER && bitbake -c cleanall victor anki-version rampost base-files wired iptables machine-robot-image core-image-anki-initramfs && $ToDo && export OTA_MANIFEST_SIGNING_KEY=$OTA_SIGNING_PASSWORD && ANKIDEV=$ADEV DO_SIGN=$DO_SIGN make && exit 0"
 
 echo
 echo -e "\033[1;32mComplete. Your $PRODorOSKR OTA is in $(pwd)/_build/$OTA_NAME.\033[0m"
