0c2fed1612a5b4db4b92e94eac09b684eb10836e
Kerigan <kerigancreighton@gmail.com>
2025-03-25 12:38:52 -0500

    temp: remove vector-gobot

--- anki/wired/vector-gobot/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CC="$(pwd)/vic-toolchain/arm-linux-gnueabi/bin/arm-linux-gnueabi-gcc" \
-CGO_LDFLAGS="-L$(pwd)/build -L$(pwd)/build/libjpeg-turbo/lib" \
-GOARM=7 \
-GOARCH=arm \
-CGO_ENABLED=1 \
-go build -o main $@
